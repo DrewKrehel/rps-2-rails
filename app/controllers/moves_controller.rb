c8e04c4f11c7ab813cae116520e5c0156156a119
Drew Krehel <drewkrehel@gmail.com>
2025-08-30 01:03:08 +0000

    i don't know

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -5,6 +5,18 @@ class MovesController < AppllicationController
   
     @player_moves = params.fetch("hand")
 
+    if @comp_moves == @player_moves
+      @outcome = "tied"
+    elsif @comp_moves == "rock" && @player_moves == "scissors"
+      @outcome = "lost"
+    elsif @comp_moves == "paper" && @player_moves == "rock"
+      @outcome = "lost"
+    elsif @comp_moves == "scissors" && @player_moves == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
     render ({ :template => "game_templates/flexible.erb"})
   end
 end
